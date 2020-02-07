e00a2766676b4e3ad20519bdb734c07eb9bf41de
unknown <edithlar726@gmail.com>
2020-02-07 14:47:22 -0600

    1er commit

---
+++ accediendo-valores-array.js
@@ -0,0 +1,7 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+// Your code here!
+
+const food = ['apple', 'pizza', 'pear']
+
+console.log(food[1])
---
+++ arrays.js
@@ -0,0 +1,7 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+// Your code here!
+
+const pizzaToppings = ["tomato sauce", "cheese", "pepperoni"];
+
+console.log(pizzaToppings);
